0f4864f180a5b39dc1d95e7429164a171d827085
arifimran5 <arifimran505@gmail.com>
2023-08-20 00:47:24 +0530

    hydration-warning supress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,26 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import React from "react";
-import { ClerkProvider } from "@clerk/nextjs";
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import React from 'react'
+import { ClerkProvider } from '@clerk/nextjs'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Board.",
-  description: "Board by listed",
-};
+  title: 'Board.',
+  description: 'Board by listed',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html suppressHydrationWarning lang='en'>
         <body>{children}</body>
       </html>
     </ClerkProvider>
-  );
+  )
 }
